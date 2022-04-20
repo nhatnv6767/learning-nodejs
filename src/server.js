c80ce121df2a8565ceb33d9557a75cbd628cd5d9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-21 01:44:16 +0900

    #6 view engine

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const path = require('path')
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
 const app = express()
 const port = 3000
 
+configViewEngine(app)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
+
 })
 
 app.listen(port, () => {
