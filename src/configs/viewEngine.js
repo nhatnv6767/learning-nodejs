e15f41b47864059f5a710d40f56073721670ba7d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-21 02:08:59 +0900

    #7 Static files: public static file

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 const configViewEngine = (app) => {
+    // nơi mà muốn cho thế giới bên ngoài có thể thấy được
+    app.use(express.static('public'))
     app.set("view engine", "ejs")
     app.set("views", "./src/views")
 }
