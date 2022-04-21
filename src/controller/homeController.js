da42cb82aaa3478fa1e623e9a809dabee302025c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-21 22:31:13 +0900

    #9 HTTP Request - mySQL connect config with MVC

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,7 +2,19 @@ import connection from '../configs/connectDB'
 
 let getHomePage = (req, res) => {
     // logic
-    return res.render('test/index.ejs')
+
+    // simple query
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            console.log('>>>> Check mysql')
+            console.log(results); // results contains rows returned by server
+
+            data = results.map((row) => { return row })
+        }
+    );
+    return res.render('test/index.ejs', { dataUser: JSON.stringify(data) });
 }
 
 module.exports = {
