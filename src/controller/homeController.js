8ddcb2b65b1d7c841b8ee46091e0cf5676aef3eb
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-21 22:40:11 +0900

    #9 HTTP Request - fix bug

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -9,11 +9,22 @@ let getHomePage = (req, res) => {
         'SELECT * FROM `users` ',
         function (err, results, fields) {
             console.log('>>>> Check mysql')
-            console.log(results); // results contains rows returned by server
+            // console.log(results); // results contains rows returned by server
 
-            data = results.map((row) => { return row })
+            data = results.map((row) => {
+                data.push({
+                    id: row.id,
+                    email: row.email,
+                    address: row.address,
+                    firstName: row.firstName,
+                    lastName: row.lastName
+                })
+            })
+
         }
     );
+
+    console.log('>>>> Check data:', data);
     return res.render('test/index.ejs', { dataUser: JSON.stringify(data) });
 }
 
