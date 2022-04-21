85dafa9893868162c48a2f31a0e4ebc846be29ff
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-21 20:18:26 +0900

    #8 Routers & MVC Design Pattern 2

--- src/route/web.js
+++ src/route/web.js
@@ -2,7 +2,11 @@ import express from 'express';
 
 let router = express.Router();
 const initWebRoute = (app) => {
+    router.get('/', (req, res) => {
+        res.render('test/index.ejs')
+    })
 
+    return app.use('/', router)
 }
 
 module.exports = initWebRoute;
--- src/server.js
+++ src/server.js
@@ -6,9 +6,6 @@ const app = express()
 const port = process.env.PORT || 8081;
 
 configViewEngine(app)
-app.get('/', (req, res) => {
-    res.render('test/index.ejs')
-})
 
 app.listen(port, () => {
     console.log(`listening at http://localhost: ${port}`)
