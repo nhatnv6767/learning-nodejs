5cfb82206e7b29a91c9ca22d9bc6e55d3f08865b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-25 21:42:00 +0900

    #11 Router params _ 3

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,6 +2,7 @@ import pool from '../configs/connectDB'
 
 let getHomePage = async (req, res) => {
 
+    
     const [rows, fields] = await pool.execute('SELECT * FROM users');
     return res.render('index.ejs', { dataUser: rows });
 
--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,8 @@ let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage)
 
+    router.get('/detail/user/:userId', homeController.getDetailPage)
+
     router.get('/about', (req, res) => {
         res.send(`I'm rich!`)
     })
