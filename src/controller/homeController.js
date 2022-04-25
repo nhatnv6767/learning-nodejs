8d0e42bbfd08a68ce7e1a24e48beb8bf5e1889a6
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-25 21:43:07 +0900

    #11 Router params _ hello detail page

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,12 +2,16 @@ import pool from '../configs/connectDB'
 
 let getHomePage = async (req, res) => {
 
-    
+
     const [rows, fields] = await pool.execute('SELECT * FROM users');
     return res.render('index.ejs', { dataUser: rows });
 
 }
 
+let getDetailPage = (req, res) => {
+    return res.send("Hello Detail Page")
+}
+
 module.exports = {
-    getHomePage
+    getHomePage, getDetailPage
 }
