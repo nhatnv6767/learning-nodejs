01bc771cd32e95c65a9ff6c9878fbb64f84864fc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-25 19:35:41 +0900

    #11 Router params _ clear

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,31 +1,9 @@
 import pool from '../configs/connectDB'
 
 let getHomePage = async (req, res) => {
-    // logic
-
-    // simple query
-    let data = [];
-    // connection.query(
-    //     'SELECT * FROM `users` ',
-    //     function (err, results, fields) {
-
-    //         results.map((row) => {
-    //             data.push({
-    //                 id: row.id,
-    //                 email: row.email,
-    //                 address: row.address,
-    //                 firstName: row.firstName,
-    //                 lastName: row.lastName
-    //             })
-    //         })
-    //         // return res.render('index.ejs', { dataUser: data });
-    //     }
-    // );
-
 
     const [rows, fields] = await pool.execute('SELECT * FROM users');
     return res.render('index.ejs', { dataUser: rows });
-
 
 }
 
