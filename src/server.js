d22487f81fe9f1c45256253aada8f87ec8bf1ce2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-26 20:39:55 +0900

    #12 Form and Post Request how to send data to server

--- src/server.js
+++ src/server.js
@@ -8,6 +8,11 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 8087;
 
+// hỗ trợ gửi data từ client lên server
+// để có thể lấy data dễ dàng
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // setup view engine
 configViewEngine(app)
 
