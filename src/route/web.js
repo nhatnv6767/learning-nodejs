bb60c91e9a0d52435261d383f6081abefe6bfb7a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-27 20:58:09 +0900

    #13 CRUD 2

--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,7 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage)
     router.get('/detail/user/:id', homeController.getDetailPage)
     router.post('/create-new-user', homeController.createNewUser)
+    router.post('/delete-user', homeController.deleteUser)
     router.get('/about', (req, res) => {
         res.send(`I'm rich!`)
     })
