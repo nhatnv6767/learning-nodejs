b3c0970273bf418e4a3cdb36b43cac0c836b6137
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-30 14:12:41 +0900

    #14 RESTfull API , API update and delete 2

--- src/controller/APIController.js
+++ src/controller/APIController.js
@@ -26,6 +26,14 @@ let createNewUser = async (req, res) => {
     })
 }
 
+let updateUser = async (req, res) => {
+
+}
+
+let deleteUser = async (req, res) => {
+
+}
+
 module.exports = {
-    getAllUsers, createNewUser
+    getAllUsers, createNewUser, updateUser, deleteUser
 }
--- src/route/api.js
+++ src/route/api.js
@@ -5,8 +5,8 @@ let router = express.Router();
 const initAPIRoute = (app) => {
     router.get('/users', APIController.getAllUsers) // Read
     router.post('/create-user', APIController.createNewUser) // Create 
-    router.put('/update-user', APIController.updateUser) // Update
-    router.delete('/delete-user', APIController.deleteUser) // Delete
+    router.put('/update-user/:id', APIController.updateUser) // Update
+    router.delete('/delete-user/:id', APIController.deleteUser) // Delete
 
     return app.use('/api/v1/', router)
 }
