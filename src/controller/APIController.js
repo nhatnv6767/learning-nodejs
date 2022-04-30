458eed494430a82a377c427a890bcd8f984668f8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-30 15:05:42 +0900

    #14 RESTfull API , API update and delete 3

--- src/controller/APIController.js
+++ src/controller/APIController.js
@@ -3,7 +3,7 @@ import pool from '../configs/connectDB'
 let getAllUsers = async (req, res) => {
     const [rows, fields] = await pool.execute('SELECT * from users')
     return res.status(200).json({
-        message: 'ok',
+        message: 'get all user ok',
         data: rows,
     })
 }
@@ -21,17 +21,31 @@ let createNewUser = async (req, res) => {
         [firstName, lastName, email, address])
 
     return res.status(200).json({
-        message: 'ok',
+        message: 'create user ok',
 
     })
 }
 
 let updateUser = async (req, res) => {
 
+    let { firstName, lastName, email, address, id } = req.body;
+    if (!firstName || !lastName || !email || !address) {
+        return res.status(200).json({
+            message: 'missing required params'
+        })
+    }
+    await pool.execute('UPDATE users SET firstName = ?, lastName = ?, email = ?, address = ? where id = ?',
+        [firstName, lastName, email, address, id])
+
+    return res.status(200).json({
+        message: 'update user ok',
+    })
 }
 
 let deleteUser = async (req, res) => {
-
+    return res.status(200).json({
+        message: 'delete user ok',
+    })
 }
 
 module.exports = {
