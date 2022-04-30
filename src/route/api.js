96079646b90c964507e3eaa873b4d458c89b08a3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-30 13:35:36 +0900

    #14 RESTfull API , API create user 2

--- src/route/api.js
+++ src/route/api.js
@@ -4,6 +4,7 @@ import APIController from '../controller/APIController'
 let router = express.Router();
 const initAPIRoute = (app) => {
     router.get('/users', APIController.getAllUsers)
+    router.post('/create-user', APIController.createNewUser)
 
     return app.use('/api/v1/', router)
 }
