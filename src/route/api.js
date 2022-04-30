7ee8811ad33aefe2fc7ffd33ebd5b163a54e19db
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-30 11:31:02 +0900

    #14 RESTfull API , API.js

--- src/route/api.js
+++ src/route/api.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import APIController from '../controller/APIController'
 
 let router = express.Router();
 const initAPIRoute = (app) => {
-    router.get('/', homeController.getHomePage)
+    router.get('/users', APIController.getAllUsers)
 
     return app.use('/api/v1/', router)
 }
