79a97948ccc26876464746bf3e269befb18deb72
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-05 18:16:56 +0900

    #15-2 Upload Multiple File FIX BUGS 4

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -51,37 +51,20 @@ let getUploadFilePage = async (req, res) => {
 }
 
 
-
-const upload = multer().single('profile-pic')
-
-// const uploadMultiple = multer().array('multiple_images', 3)
-
-
 let handleUploadFile = async (req, res) => {
     // 'profile_pic' is the name of our file input field in the HTML form
     // profile_pic mapping with uploadFile.ejs
-    // let upload = multer({ storage: storage, fileFilter: imageFilter }).single('profile_pic');
 
-    upload(req, res, function (err) {
-        // req.file contains information of uploaded file
-        // req.body contains information of text fields, if there were any
 
-        if (req.fileValidationError) {
-            return res.send(req.fileValidationError);
-        }
-        else if (!req.file) {
-            return res.send('Please select an image to upload');
-        }
-        else if (err instanceof multer.MulterError) {
-            return res.send(err);
-        }
-        else if (err) {
-            return res.send(err);
-        }
+    if (req.fileValidationError) {
+        return res.send(req.fileValidationError);
+    }
+    else if (!req.file) {
+        return res.send('Please select an image to upload');
+    }
 
-        // Display uploaded image for user validation
-        res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
-    });
+    // Display uploaded image for user validation
+    res.send(`You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`);
 }
 
 let handleUploadMultipleFiles = async (req, res) => {
