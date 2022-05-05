99805a4e48c9a26af55f02c16e85f4a536d803c8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-05 13:24:43 +0900

    #15 Upload single file Web -> Home Fix bug 2

--- src/route/web.js
+++ src/route/web.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import homeController from '../controller/homeController'
+import multer from 'multer';
+import path from 'path';
 
+var appRoot = require('app-root-path');
 let router = express.Router();
+
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage)
     router.get('/detail/user/:id', homeController.getDetailPage)
