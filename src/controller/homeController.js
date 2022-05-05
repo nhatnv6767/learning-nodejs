75a5ea16dd472c9c2cbe56693428611a89e3af48
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-05 12:51:15 +0900

    #15 Upload single file 14

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -62,8 +62,33 @@ const storage = multer.diskStorage({
 });
 
 let handleUploadFile = async (req, res) => {
-
+    // 'profile_pic' is the name of our file input field in the HTML form
+    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
 }
+
+app.post('/upload-profile-pic', (req, res) => {
+
+    upload(req, res, function (err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
+    });
+});
 
 module.exports = {
     getHomePage, getDetailPage, createNewUser, deleteUser, getEditUser, postUpdateUser,
