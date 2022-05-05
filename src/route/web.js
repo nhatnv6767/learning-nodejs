916616e8cd7ffe690bf936a13faa038300a8dc40
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-05 13:29:02 +0900

    #15 Upload single file Web -> Home Fix bug 3

--- src/route/web.js
+++ src/route/web.js
@@ -6,6 +6,28 @@ import path from 'path';
 var appRoot = require('app-root-path');
 let router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, appRoot + "/src/public/image/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+// middleware
+let upload = multer({ storage: storage, fileFilter: imageFilter })
+
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomePage)
     router.get('/detail/user/:id', homeController.getDetailPage)
